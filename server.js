23091a793917a79d7d4ca50d57bdd0493b467084
Kaushal Kishor Singh <kaushalsingh1720@gmail.com>
2026-03-08 08:42:25 +0530

    minor changes

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/health', (req, res) => {
+    res.json({Connection:"Healthy"});
 })
 app.use('/auth', authRoutes);
 app.use(errorHandler);
